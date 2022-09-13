a9afd4855847c7bde7867627f6cae73f17f59daa
nguyentbgcd201888 <nguyentbgcd201888@fpt.edu.vn>
2022-09-13 21:23:27 +0700

    Create edit user function

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -44,5 +44,71 @@ public class UsersController : Controller
         }
         return View(obj);
     }
+    //GET
+    public IActionResult Edit(int? id)
+    {
+        if (id == null||id==0)
+        {
+            return NotFound();
+        }
+        var UserFormDb = _db.Users.Find(id);
+        // var CategoryFormDbFirst = _db.Categories.FirstOrDefault(u=>u.Id==id);
+        // var CategoryFormDbSingle = _db.Categories.SingleOrDefault(u=>u.Id==id);
+        if (UserFormDb==null)
+        {
+            return NotFound();
+        }
+        return View(UserFormDb);
+    }
     
+    //POST
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Edit(User obj)
+    {
+        // if (obj.Name==obj.DisplayOder.ToString())
+        // {
+        //     ModelState.AddModelError("CustomError","The DisplayOrder cannot exactly match the Name");
+        // }
+        if (ModelState.IsValid)
+        {
+            _db.Users.Update(obj);
+            _db.SaveChanges(); 
+            TempData["Success"] = "Category updated  successfully";
+
+            return RedirectToAction("Index");
+        }
+        return View(obj);
+    }
+    
+    public IActionResult Delete(int? id)
+    {
+        if (id == null||id==0)
+        {
+            return NotFound();
+        }
+        var userFormDb = _db.Users.Find(id);
+       
+        if (userFormDb==null)
+        {
+            return NotFound();
+        }
+        return View(userFormDb);
+    }
+    
+    //POST
+    [HttpPost,ActionName("Delete")]//Change action name(DeleteCategoryPOST -> Delete)
+    [ValidateAntiForgeryToken]
+    public IActionResult DeleteUserPost(int? id)
+    {
+        var obj = _db.Categories.Find(id);
+        if (obj==null)
+        {
+            return NotFound();
+        }
+        _db.Categories.Remove(obj);
+        _db.SaveChanges(); 
+        TempData["Success"] = "Category deleted  successfully";
+        return RedirectToAction("Index");
+    }
 }
